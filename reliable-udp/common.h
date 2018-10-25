cd7de604090cfb8527250fb37d2aa144dd256316
djp <duanjp8617@gmail.com>
2018-10-25 19:52:28 +0800

    Update port

--- reliable-udp/common.h
+++ reliable-udp/common.h
@@ -1,8 +1,8 @@
 #define SERVER_PORT 8888    //The port on which to listen for incoming data
-#define SERVER_ADDR "127.0.0.1"
+#define SERVER_ADDR "192.168.122.11"
 
 #define CLIENT_PORT 9999
-#define CLIENT_ADDR "127.0.0.1"
+#define CLIENT_ADDR "192.168.122.12"
 
 #define MSG_SIZE 2000
 #define BUFLEN 4096  //Max length of buffer
